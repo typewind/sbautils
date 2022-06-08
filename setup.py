6d3a9db7331667e9b69a3cfa1fd09105fddbd65e
ChenShuyao <cshuyaoxx@gmail.com>
2022-06-08 13:19:07 +0100

    Made a package and move some files

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sba_utlis",
+    version="0.0.1",
+    author="Typewind",
+    author_email="indi@juntoalmanzanares.futbol",
+    description="Personal Statsbomb data analysis utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/typewind/sba_utils",
+    project_urls={
+        "Bug Tracker": "https://github.com/typewind/sba_utils/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
