420ed701f5df353a7ac28826081ba597de8c6923
ChenShuyao <cshuyaoxx@gmail.com>
2022-06-08 14:07:08 +0100

    Rename package to sbautlis

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sba_utlis",
+    name="sbautlis",
     version="0.0.1",
     author="Typewind",
     author_email="indi@juntoalmanzanares.futbol",
     description="Personal Statsbomb data analysis utility functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/typewind/sba_utils",
+    url="https://github.com/typewind/sbautils",
     project_urls={
-        "Bug Tracker": "https://github.com/typewind/sba_utils/issues",
+        "Bug Tracker": "https://github.com/typewind/sbautils/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
